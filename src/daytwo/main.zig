072aa650e79bf724a9d006748371ab9ce85f659d
Exiles.prx <exiles.prx@gmail.com>
2023-12-09 00:25:26 -0500

    Part two

--- src/daytwo/main.zig
+++ src/daytwo/main.zig
@@ -9,6 +9,7 @@ pub fn main() !void {
         green = 13,
         blue = 14,
     };
+    _ = Bag;
 
     const filename: []const u8 = "input.txt";
     const file = try std.fs.cwd().openFile(filename, .{});
@@ -24,19 +25,20 @@ pub fn main() !void {
 
     var iter = std.mem.splitSequence(u8, readBuf, "\n");
 
-    var sum: u16 = 0;
-    var gameNum: u8 = 1;
+    var total: u32 = 0;
     while (iter.next()) |line| {
         // std.debug.print("{s}\n", .{line});
-        var games = std.mem.splitAny(u8, line, ":;,");
-        var isValidGame = true;
-        while (games.next()) |game| {
+        var sets = std.mem.splitAny(u8, line, ":;,");
+        var redCubes: u8 = 0;
+        var greenCubes: u8 = 0;
+        var blueCubes: u8 = 0;
+        while (sets.next()) |details| {
             // std.debug.print("{s}\n", .{game});
             var num = std.ArrayList(u8).init(allocator);
             var string = std.ArrayList(u8).init(allocator);
             defer num.deinit();
             defer string.deinit();
-            for (game) |token| {
+            for (details) |token| {
                 if (std.ascii.isDigit(token)) {
                     try num.append(token);
                 } else if (std.ascii.isAlphabetic(token)) {
@@ -44,28 +46,24 @@ pub fn main() !void {
                 }
             }
 
-            const convertedNum = try std.fmt.parseInt(u8, std.mem.sliceTo(num.items, 0), 10);
-            const convertedString = std.mem.sliceTo(string.items, 0);
+            const cubes = try std.fmt.parseInt(u8, std.mem.sliceTo(num.items, 0), 10);
+            const color = std.mem.sliceTo(string.items, 0);
 
-            if (std.mem.eql(u8, convertedString, "red") and convertedNum > @intFromEnum(Bag.red)) {
-                isValidGame = false;
+            if (std.mem.eql(u8, color, "red") and cubes > redCubes) {
+                redCubes = cubes;
             }
-            if (std.mem.eql(u8, convertedString, "green") and convertedNum > @intFromEnum(Bag.green)) {
-                isValidGame = false;
+            if (std.mem.eql(u8, color, "green") and cubes > greenCubes) {
+                greenCubes = cubes;
             }
-            if (std.mem.eql(u8, convertedString, "blue") and convertedNum > @intFromEnum(Bag.blue)) {
-                isValidGame = false;
+            if (std.mem.eql(u8, color, "blue") and cubes > blueCubes) {
+                blueCubes = cubes;
             }
         }
-
-        if (isValidGame) {
-            sum += gameNum;
-        }
-
-        gameNum += 1;
+        std.debug.print("red: {d}, green: {d}, blue: {d}\n", .{ redCubes, greenCubes, blueCubes });
+        total += @as(u16, @intCast(redCubes)) * @as(u16, @intCast(greenCubes)) * @as(u16, @intCast(blueCubes));
     }
 
-    std.debug.print("Sum: {d}\n", .{sum});
+    std.debug.print("total: {d}\n", .{total});
 }
 
 test "concat char to string" {
